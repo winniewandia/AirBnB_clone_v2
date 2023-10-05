5eb0ef5e7e27d01c40822a8b211285a1c02b6a6d
winniewandia <winniewandia63@gmail.com>
2023-10-05 10:56:00 +0300

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Compress before sending
+"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """Function that compresses a folder
+
+    Returns:
+        str: archive name
+    """
+    try:
+        local("mkdir -p versions")
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_name = "versions/web_static_{}.tgz".format(timestamp)
+        local("tar -cvzf {} web_static".format(archive_name))
+        return archive_name
+    except Exception:
+        return None
