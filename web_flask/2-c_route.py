3f09bf50635b10634dccc84e1e02c0f8d6619747
winniewandia <winniewandia63@gmail.com>
2023-10-20 10:46:00 +0300

    flask web app with 3 routes

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""A script that starts a Flask web application and has 2 routes
+"""A script that starts a Flask web application and has 3 routes
 """
+from markupsafe import escape
 from flask import Flask
 
 app = Flask(__name__)
@@ -21,6 +22,19 @@ def hbnb():
     """function to display HBNB"
     """
     return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """display “C ” followed by the value of the text variable
+
+    Args:
+        text (str): user input
+
+    Returns:
+        web application
+    """
+    return f'C {escape(text)}'
 
 
 if __name__ == "__main__":
--- web_flask/README.md
+++ web_flask/README.md
@@ -1,2 +1,3 @@
 0. Creating a script that starts a flask web application
 1. the flask web application with 2 routes
+2. flask web app with 3 routes
